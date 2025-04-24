2aa7c1bf8cd79c8f0bc7bba5430e0b33ee8858f4
The_Pr0fessor <46141975+TrendonRobinson@users.noreply.github.com>
2025-04-23 23:30:49 -0400

    rbxts-router v1.0.4

--- Formula/rbxts-router.rb
+++ Formula/rbxts-router.rb
@@ -2,7 +2,7 @@ class RbxtsRouter < Formula
     desc "CLI tools for @rbxts/router"
     homepage "https://github.com/Whimco-Studio/homebrew-rbxts-router/"
     url "https://github.com/Whimco-Studio/homebrew-rbxts-router/releases/download/v1.0.4/rbxts-router-macos-arm64"
-    sha256 "23869a5b759c446116b3a8b54eadcb95cd46d72a13feb73f4c87c8a9e1a21240"
+    sha256 "fce673618a4abbce0ef25940a0442ab9239c2ea38271609a2d783df053cb7036"
     version "1.0.4"
     license "ISC"
   
