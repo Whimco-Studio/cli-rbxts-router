ec3b127b871f39c9c221f6887d95a5c44ffd25d9
The_Pr0fessor <46141975+TrendonRobinson@users.noreply.github.com>
2025-04-23 21:04:46 -0400

    Update Homebrew formula for rbxts-router to reflect new homepage URL

--- Formula/rbxts-router.rb
+++ Formula/rbxts-router.rb
@@ -1,7 +1,7 @@
 class RbxtsRouter < Formula
     desc "CLI tools for @rbxts/router"
-    homepage "https://github.com/Whimco-Studio/cli-rbxts-router/"
-    url "https://github.com/Whimco-Studio/cli-rbxts-router/releases/download/v1.0.1/rbxts-router-macos-arm64"
+    homepage "https://github.com/Whimco-Studio/homebrew-rbxts-router/"
+    url "https://github.com/Whimco-Studio/homebrew-rbxts-router/releases/download/v1.0.1/rbxts-router-macos-arm64"
     sha256 "a791e4847bcb9d462a7f140b624cdf5274e1f1e0af7095bcb31a0a27b439b6fd"
     version "1.0.1"
     license "ISC"
