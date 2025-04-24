4559101393d530085823a09f39bf9cbeb5e878aa
The_Pr0fessor <46141975+TrendonRobinson@users.noreply.github.com>
2025-04-23 23:27:10 -0400

    Update rbxts-router formula to version 1.0.4

--- Formula/rbxts-router.rb
+++ Formula/rbxts-router.rb
@@ -3,7 +3,7 @@ class RbxtsRouter < Formula
     homepage "https://github.com/Whimco-Studio/homebrew-rbxts-router/"
     url "https://github.com/Whimco-Studio/homebrew-rbxts-router/releases/download/v1.0.3/rbxts-router-macos-arm64"
     sha256 "23869a5b759c446116b3a8b54eadcb95cd46d72a13feb73f4c87c8a9e1a21240"
-    version "1.0.3"
+    version "1.0.4"
     license "ISC"
   
     def install
